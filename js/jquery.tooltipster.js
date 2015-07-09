c9405eee45792b91a8fffbebdee876de7d17db45
Alexey Svetliakov <asvetliakov@teledini.com>
2015-07-09 05:08:11 +0200

    require for commonjs

--- js/jquery.tooltipster.js
+++ js/jquery.tooltipster.js
@@ -9,7 +9,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 */
 
-;(function ($, window, document) {
+var jQuery = require('jquery');
+//;(function ($, window, document) {
 
 	var pluginName = "tooltipster",
 		defaults = {
@@ -1324,4 +1325,4 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 		}
 		return false;
 	}
-})( jQuery, window, document );
+//})( jQuery, window, document );
